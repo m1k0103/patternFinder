d1eb401d2cb509f287b2b96d37aea98ddb34f438
mikok <84585119+miko1034@users.noreply.github.com>
2025-08-10 22:00:23 +0100

    in my head it should work, but now printing it to see if it works throws errors

--- pattern.c
+++ pattern.c
@@ -1,26 +1,29 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 
-char getUniqueChars(char string){
+char *getUniqueChars(char* string){
     // Count how many unique items there are
     int stringLen = strlen(string);
-    char final[] = "";
     // Cycle though all characters
     for (int i=0; i<stringLen; i++){
         // Cycle through them again and compare each second cycle letter with first cycle letter
         for (int j=0; j<i; j++){
-
+            if ((string[i] == string[j]) || (i != j)){
+                //printf("removed %s", string[j]);
+                printf(string);
+                string[j] = '\0';
+            }
         }
-
     }
-
-
+    printf("%s", strlen(string));
+    return string;
 }
 
 int main(){
     char text[] = "kgoahkrhgrkrrorghaoargohorakoaoahrhoohkarkoorrrokaggahrogoagaoka";
-    printf(getUniqueChars(text));
-
+    printf("%s", getUniqueChars(text));
+    //printf("test");
     return 0;
 }
